060878bc72db91f75ac68ab26e8ec8059a4d3884
林镇洪 <253347668@qq.com>
2020-07-22 17:36:42 +0800

    fix the Address bug

--- common/custom.types.161.js
+++ common/custom.types.161.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenId = exports.RawAmount = void 0;
+exports.TokenId = exports.RawAmount = exports.Address = void 0;
 const codec_1 = require("@polkadot/types/codec");
 const primitive_1 = require("@polkadot/types/primitive");
+const generic_1 = require("@polkadot/types/generic");
+class Address extends generic_1.GenericAccountId {
+}
+exports.Address = Address;
 class RawAmount extends codec_1.UInt {
     constructor(registry, value) {
         super(registry, value, 128);
